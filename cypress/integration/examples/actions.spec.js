57d2eab27178b3e478d1991ffdd0f870a3f1de91
gaurav8760 <65512730+gaurav8760@users.noreply.github.com>
2021-11-06 02:19:00 +0530

    Update actions.spec.js

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -58,7 +58,7 @@ context('Actions', () => {
   it('.submit() - submit a form', () => {
     // https://on.cypress.io/submit
     cy.get('.action-form')
-      .find('[type="text"]').type('HALFOFF')
+      .find('[type="text"]').type('OFFHALL')
 
     cy.get('.action-form').submit()
       .next().should('contain', 'Your form has been submitted!')
